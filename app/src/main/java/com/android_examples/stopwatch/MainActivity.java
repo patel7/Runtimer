ffec01e62c86c76bc523b24aac50b51eab35b104
NobleTalal <nobletalal@hotmail.com>
2017-01-29 22:51:25 -0800

    Talal's text commit

--- app/src/main/java/com/android_examples/stopwatch/MainActivity.java
+++ app/src/main/java/com/android_examples/stopwatch/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Talal comment test
     TextView textView ;
 
     Button start, pause, reset, lap ;
