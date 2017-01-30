5ae06e1f05b0f6cda1981778e756715f04e311de
Ryan Patel <supatel@live.com>
2017-01-29 23:04:41 -0800

    Initial commit of prototype progress

--- app/src/main/java/com/android_examples/stopwatch/MainActivity.java
+++ app/src/main/java/com/android_examples/stopwatch/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     // Talal comment test
+    // Ryan comment test
     TextView textView ;
 
     Button start, pause, reset, lap ;
