e3646c671784da439c68db3d510adb36975922c6
Ryan Patel <supatel@live.com>
2017-03-03 21:03:29 -0800

    Began work selector for 5k and 10k races when uploading to database

--- app/src/main/java/com/android_examples/stopwatch/MainActivity.java
+++ app/src/main/java/com/android_examples/stopwatch/MainActivity.java
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
         alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
+                // dialog.cancel();
             }
         });
 
